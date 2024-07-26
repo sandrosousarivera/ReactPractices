41d385999cfec55256a63dc2dbd878d28d2d782e
Sandro Sousa Rivera <sandro@SSs-MacBook-Pro.local>
2024-07-26 15:13:14 +0200

    Project creation

---
+++ todolist/src/App.jsx
@@ -0,0 +1,7 @@
+import TodoApp from "./components/TodoApp.jsx";
+
+function App() {
+  return <TodoApp />;
+}
+
+export default App;
---
+++ todolist/src/components/todo.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+export default function Todo({ item }) {
+  const [isEdit, setIsEdit] = useState(false);
+  return (
+    <div>
+      {item.title}
+      <button onClick={() => setIsEdit(true)}>Edit</button>
+      <button>Delete</button>
+    </div>
+  );
+}
