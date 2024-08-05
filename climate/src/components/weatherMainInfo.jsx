ed1d49de88a28780f7d4f750dcbcf4a4bcaf91e0
Sandro Sousa Rivera <sandro@SSs-MacBook-Pro.local>
2024-08-05 11:02:43 +0200

    Map longitude latitude update

--- climate/src/components/weatherMainInfo.jsx
+++ climate/src/components/weatherMainInfo.jsx
@@ -16,6 +16,17 @@ export default function WeatherMainInfo({ weather }) {
           <div>{weather?.current.temp_c}º</div>
         </div>
       </div>
+      <iframe
+        src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d317716.60640415194!2d${weather?.location.lon}944!3d${weather?.location.lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1
+        s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondres%2C%20Reino%20Unido!5e0!3m2!1ses!2ses!4v
+        1722847018741!5m2!1ses!2ses`}
+        width="600"
+        height="450"
+        style={{ border: "0" }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
     </div>
   );
 }
