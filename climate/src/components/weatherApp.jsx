aa7865040c9b622b820fd89ebc62342a008a91d0
Sandro Sousa Rivera <sandro@SSs-MacBook-Pro.local>
2024-08-05 13:11:55 +0200

    Creating style modules

--- climate/src/components/weatherApp.jsx
+++ climate/src/components/weatherApp.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import WeatherForm from "./weatherForm";
 import WeatherMainInfo from "./weatherMainInfo";
+import styles from "./weatherApp.module.css";
 
 export default function WeatherApp() {
   const [weather, setWeather] = useState(null);
@@ -44,7 +45,7 @@ export default function WeatherApp() {
   }
 
   return (
-    <div>
+    <div className={styles.weatherContainer}>
       <WeatherForm onChangeCity={handleChangeCity} />
       <WeatherMainInfo weather={weather} />
     </div>
