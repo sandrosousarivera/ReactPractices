68f3df1e450f4876b80bba1e1a497659b0072a3c
Sandro Sousa Rivera <sandro@SSs-MacBook-Pro.local>
2024-08-05 14:26:37 +0200

    Sdding style to components

--- climate/src/components/weatherMainInfo.jsx
+++ climate/src/components/weatherMainInfo.jsx
@@ -1,9 +1,11 @@
+import styles from "../weatherMainInfo.module.css";
+
 export default function WeatherMainInfo({ weather }) {
   return (
-    <div>
-      <div>{weather?.location.name}</div>
-      <div>{weather?.location.country}</div>
-      <div>
+    <div className={styles.mainInfo}>
+      <div className={styles.city}>{weather?.location.name}</div>
+      <div className={styles.country}>{weather?.location.country}</div>
+      <div className={styles.row}>
         <div>
           <img
             src={`http:${weather?.current.condition.icon}`}
@@ -11,16 +13,18 @@ export default function WeatherMainInfo({ weather }) {
             alt={weather?.current.condition.text}
           />
         </div>
-        <div>
-          <div>{weather?.current.condition.text}</div>
-          <div>{weather?.current.temp_c}º</div>
+        <div className={styles.weatherConditions}>
+          <div className={styles.condition}>
+            {weather?.current.condition.text}
+          </div>
+          <div className={styles.current}>{weather?.current.temp_c}º</div>
         </div>
       </div>
       <iframe
         src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d317716.60640415194!2d${weather?.location.lon}944!3d${weather?.location.lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1
         s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondres%2C%20Reino%20Unido!5e0!3m2!1ses!2ses!4v
         1722847018741!5m2!1ses!2ses`}
-        width="600"
+        width="100%"
         height="450"
         style={{ border: "0" }}
         allowfullscreen=""
