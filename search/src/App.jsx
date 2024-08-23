df83a360ed4b9809797397b59310385c91be2514
Sandro Sousa Rivera <sandro@SSs-MacBook-Pro.local>
2024-08-23 11:33:26 +0200

    Adding buttons and mapping data

--- search/src/App.jsx
+++ search/src/App.jsx
@@ -252,7 +252,62 @@ const emails = [
 function App() {
   const [data, setData] = useState([...people, ...calendar, ...emails]);
 
-  return <div>Hola mundo</div>;
+  const [selection, setSelection] = useState(null);
+  const [currentOption, setCurrentOption] = useState("all");
+
+  function handleClick(e) {
+    const op = e.target.name;
+
+    switch (op) {
+      case "all":
+        setData([...people, ...calendar, ...emails]);
+        setCurrentOption("all");
+
+        break;
+
+      default:
+      case "calendar":
+        setData([...calendar]);
+        setCurrentOption("calendar");
+
+        break;
+
+      case "people":
+        setData([...people]);
+        setCurrentOption("people");
+
+        break;
+
+      case "emails":
+        setData([...emails]);
+        setCurrentOption("emails");
+
+        break;
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick} name="all">
+        All
+      </button>
+      <button onClick={handleClick} name="people">
+        People
+      </button>
+      <button onClick={handleClick} name="calendar">
+        Calendar
+      </button>
+      <button onClick={handleClick} name="emails">
+        Emails
+      </button>
+
+      <div>
+        {data.map((item) => (
+          <div>{item.title}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
